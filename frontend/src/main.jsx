70a68d725b23274a2c42afc57457f80b1ac3233b
Leeni-Patidar <leenip04@gmail.com>
2026-03-23 13:50:04 +0530

    Replace BrowserRouter with HashRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from './components/ui/sonner'
 
 
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <QueryClientProvider client={queryClient}>
     <Toaster/>
     <App />
     </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
